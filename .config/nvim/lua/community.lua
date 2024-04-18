80f62ad9858800b39bffaa6a6ef60f72fd1ca9c7
Krzysztof Gasienica-Bednarz <krzysztofgb@yahoo.com>
2024-04-18 16:01:42 -0500

    Update and nv alias

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -4,174 +4,169 @@
 
 ---@type LazySpec
 return {
-	"AstroNvim/astrocommunity",
-	-- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-	{ import = "astrocommunity.colorscheme.tokyonight-nvim" },
-	{
-		"folke/tokyonight.nvim",
-		opts = {
-			style = "night",
-		},
-	},
-	-- per line commenting
-	{ import = "astrocommunity.comment.mini-comment" },
-	{ import = "astrocommunity.completion.cmp-cmdline" },
-	{ import = "astrocommunity.debugging.nvim-dap-repl-highlights" },
-	{ import = "astrocommunity.debugging.nvim-dap-virtual-text" },
-	{ import = "astrocommunity.debugging.telescope-dap-nvim" },
-	{ import = "astrocommunity.editing-support.conform-nvim" },
-	{
-		"stevearc/conform.nvim",
-		event = { "BufWritePre" },
-		cmd = { "ConformInfo" },
-		keys = {
-			{
-				"<Leader>fc",
-				function()
-					require("conform").format({ async = true, lsp_fallback = true })
-				end,
-				mode = "",
-				desc = "[F]ormat [C]urrent Buffer",
-			},
-		},
-		opts = {
-			notify_on_error = false,
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
-			formatters_by_ft = {
-				go = { "gofumpt" },
-				lua = { "stylua" },
-				python = { "black" },
-				-- Conform can also run multiple formatters sequentially
-				-- python = { "isort", "black" },
-				--
-				-- Can use a sub-list to tell conform to run *until* a formatter is found.
-				-- javascript = { { "prettierd", "prettier" } },
-			},
-		},
-	},
-	{ import = "astrocommunity.file-explorer.telescope-file-browser-nvim" },
-	{ import = "astrocommunity.fuzzy-finder.telescope-zoxide" },
-	{ import = "astrocommunity.git.blame-nvim" },
-	{ import = "astrocommunity.indent.mini-indentscope" },
-	{ import = "astrocommunity.lsp.lsp-signature-nvim" },
-	{ import = "astrocommunity.lsp.nvim-lint" },
-	{
-		"mfussenegger/nvim-lint",
-		event = { "BufReadPre", "BufNewFile" },
-		config = function()
-			local lint = require("lint")
-			lint.linters_by_ft = {
-				go = { "golangcilint" },
-				markdown = { "markdownlint" },
-			}
+  "AstroNvim/astrocommunity",
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      style = "night",
+    },
+  },
+  -- per line commenting
+  { import = "astrocommunity.comment.mini-comment" },
+  { import = "astrocommunity.completion.cmp-cmdline" },
+  { import = "astrocommunity.debugging.nvim-dap-repl-highlights" },
+  { import = "astrocommunity.debugging.nvim-dap-virtual-text" },
+  { import = "astrocommunity.debugging.telescope-dap-nvim" },
+  { import = "astrocommunity.editing-support.conform-nvim" },
+  {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+      {
+        "<Leader>fc",
+        function() require("conform").format { async = true, lsp_fallback = true } end,
+        mode = "",
+        desc = "[F]ormat [C]urrent Buffer",
+      },
+    },
+    opts = {
+      notify_on_error = false,
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+      formatters_by_ft = {
+        go = { "gofumpt" },
+        lua = { "stylua" },
+        python = { "black" },
+        -- Conform can also run multiple formatters sequentially
+        -- python = { "isort", "black" },
+        --
+        -- Can use a sub-list to tell conform to run *until* a formatter is found.
+        -- javascript = { { "prettierd", "prettier" } },
+      },
+    },
+  },
+  { import = "astrocommunity.file-explorer.telescope-file-browser-nvim" },
+  { import = "astrocommunity.fuzzy-finder.telescope-zoxide" },
+  { import = "astrocommunity.git.blame-nvim" },
+  { import = "astrocommunity.indent.mini-indentscope" },
+  { import = "astrocommunity.lsp.lsp-signature-nvim" },
+  { import = "astrocommunity.lsp.nvim-lint" },
+  {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local lint = require "lint"
+      lint.linters_by_ft = {
+        go = { "golangcilint" },
+        markdown = { "markdownlint" },
+      }
 
-			local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-				group = lint_augroup,
-				callback = function()
-					require("lint").try_lint()
-				end,
-			})
-		end,
-	},
-	{ import = "astrocommunity.lsp.nvim-lsp-file-operations" },
-	{ import = "astrocommunity.markdown-and-latex.glow-nvim" },
-	{ import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
-	{ import = "astrocommunity.media.codesnap-nvim" },
-	{ import = "astrocommunity.motion.flash-nvim" },
-	{ import = "astrocommunity.motion.harpoon" },
-	{ import = "astrocommunity.motion.mini-surround" },
-	{ import = "astrocommunity.pack.bash" },
-	{ import = "astrocommunity.pack.docker" },
-	{ import = "astrocommunity.pack.lua" },
-	{ import = "astrocommunity.pack.go" },
-	{ import = "astrocommunity.pack.java" },
-	{ import = "astrocommunity.pack.json" },
-	{ import = "astrocommunity.pack.markdown" },
-	{ import = "astrocommunity.pack.proto" },
-	{ import = "astrocommunity.pack.python" },
-	{ import = "astrocommunity.pack.rust" },
-	{ import = "astrocommunity.pack.sql" },
-	{ import = "astrocommunity.pack.terraform" },
-	{ import = "astrocommunity.pack.toml" },
-	{ import = "astrocommunity.pack.yaml" },
-	{ import = "astrocommunity.recipes.disable-tabline" },
-	{ import = "astrocommunity.startup.mini-starter" },
-	{
-		"echasnovski/mini.starter",
-		config = function(_, opts)
-			local starter = require("mini.starter")
-			starter.setup({
-				header = table.concat({
-					[[            __    __                             __]],
-					[[            |  \  |  \                           |  \]],
-					[[            | ▓▓\ | ▓▓ ______   ______  __     __ \▓▓______ ____]],
-					[[            | ▓▓▓\| ▓▓/      \ /      \|  \   /  \  \      \    \]],
-					[[            | ▓▓▓▓\ ▓▓  ▓▓▓▓▓▓\  ▓▓▓▓▓▓\\▓▓\ /  ▓▓ ▓▓ ▓▓▓▓▓▓\▓▓▓▓\]],
-					[[            | ▓▓\▓▓ ▓▓ ▓▓    ▓▓ ▓▓  | ▓▓ \▓▓\  ▓▓| ▓▓ ▓▓ | ▓▓ | ▓▓]],
-					[[            | ▓▓ \▓▓▓▓ ▓▓▓▓▓▓▓▓ ▓▓__/ ▓▓  \▓▓ ▓▓ | ▓▓ ▓▓ | ▓▓ | ▓▓]],
-					[[            | ▓▓  \▓▓▓\▓▓     \\▓▓    ▓▓   \▓▓▓  | ▓▓ ▓▓ | ▓▓ | ▓▓]],
-					[[             \▓▓   \▓▓ \▓▓▓▓▓▓▓ \▓▓▓▓▓▓     \▓    \▓▓\▓▓  \▓▓  \▓▓]],
-					[[]],
-					[[                           Welcome back, Krzysztof!               ]],
-				}, "\n"),
-				footer = "",
-				items = {
-					starter.sections.telescope(),
-					starter.sections.recent_files(),
-					starter.sections.builtin_actions(),
-				},
-				content_hooks = {
-					starter.gen_hook.adding_bullet(),
-					starter.gen_hook.aligning("center", "center"),
-				},
-			})
-		end,
-	},
-	{ import = "astrocommunity.test.neotest" },
-	{ import = "astrocommunity.utility.mason-tool-installer-nvim" },
-	{ import = "astrocommunity.utility.telescope-live-grep-args-nvim" },
-	{
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		opts = {
-			ensure_installed = {
-				black = {},
-				["golangci-lint"] = {},
-				gofumpt = {},
-				gopls = {
-					analyses = {
-						unusedvariable = true,
-					},
-				},
-				["lua_ls"] = {
-					settings = {
-						Lua = {
-							runtime = { version = "LuaJIT" },
-							workspace = {
-								checkThirdParty = false,
-								library = {
-									"${3rd}/luv/library",
-									unpack(vim.api.nvim_get_runtime_file("", true)),
-								},
-							},
-							completion = {
-								callSnippet = "Replace",
-							},
-							diagnostics = { disable = { "missing-fields" } },
-							format = {
-								enable = false,
-							},
-						},
-					},
-				},
-				markdownlint = {},
-				pyright = {},
-				stylua = {},
-			},
-		},
-	},
-	{ import = "astrocommunity.workflow.hardtime-nvim" },
+      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+        group = lint_augroup,
+        callback = function() require("lint").try_lint() end,
+      })
+    end,
+  },
+  { import = "astrocommunity.lsp.nvim-lsp-file-operations" },
+  { import = "astrocommunity.markdown-and-latex.glow-nvim" },
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  { import = "astrocommunity.media.codesnap-nvim" },
+  { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.motion.harpoon" },
+  { import = "astrocommunity.motion.mini-surround" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.proto" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.sql" },
+  { import = "astrocommunity.pack.terraform" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.recipes.disable-tabline" },
+  { import = "astrocommunity.startup.mini-starter" },
+  {
+    "echasnovski/mini.starter",
+    config = function(_, opts)
+      local starter = require "mini.starter"
+      starter.setup {
+        header = table.concat({
+          [[            __    __                             __]],
+          [[            |  \  |  \                           |  \]],
+          [[            | ▓▓\ | ▓▓ ______   ______  __     __ \▓▓______ ____]],
+          [[            | ▓▓▓\| ▓▓/      \ /      \|  \   /  \  \      \    \]],
+          [[            | ▓▓▓▓\ ▓▓  ▓▓▓▓▓▓\  ▓▓▓▓▓▓\\▓▓\ /  ▓▓ ▓▓ ▓▓▓▓▓▓\▓▓▓▓\]],
+          [[            | ▓▓\▓▓ ▓▓ ▓▓    ▓▓ ▓▓  | ▓▓ \▓▓\  ▓▓| ▓▓ ▓▓ | ▓▓ | ▓▓]],
+          [[            | ▓▓ \▓▓▓▓ ▓▓▓▓▓▓▓▓ ▓▓__/ ▓▓  \▓▓ ▓▓ | ▓▓ ▓▓ | ▓▓ | ▓▓]],
+          [[            | ▓▓  \▓▓▓\▓▓     \\▓▓    ▓▓   \▓▓▓  | ▓▓ ▓▓ | ▓▓ | ▓▓]],
+          [[             \▓▓   \▓▓ \▓▓▓▓▓▓▓ \▓▓▓▓▓▓     \▓    \▓▓\▓▓  \▓▓  \▓▓]],
+          [[]],
+          [[                           Welcome back, Krzysztof!               ]],
+        }, "\n"),
+        footer = "",
+        items = {
+          starter.sections.telescope(),
+          starter.sections.recent_files(),
+          starter.sections.builtin_actions(),
+        },
+        content_hooks = {
+          starter.gen_hook.adding_bullet(),
+          starter.gen_hook.aligning("center", "center"),
+        },
+      }
+    end,
+  },
+  { import = "astrocommunity.test.neotest" },
+  { import = "astrocommunity.utility.mason-tool-installer-nvim" },
+  { import = "astrocommunity.utility.telescope-live-grep-args-nvim" },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = {
+      ensure_installed = {
+        black = {},
+        ["golangci-lint"] = {},
+        gofumpt = {},
+        gopls = {
+          analyses = {
+            unusedvariable = true,
+          },
+        },
+        ["lua_ls"] = {
+          settings = {
+            Lua = {
+              runtime = { version = "LuaJIT" },
+              workspace = {
+                checkThirdParty = false,
+                library = {
+                  "${3rd}/luv/library",
+                  unpack(vim.api.nvim_get_runtime_file("", true)),
+                },
+              },
+              completion = {
+                callSnippet = "Replace",
+              },
+              diagnostics = { disable = { "missing-fields" } },
+              format = {
+                enable = false,
+              },
+            },
+          },
+        },
+        markdownlint = {},
+        pyright = {},
+        stylua = {},
+      },
+    },
+  },
 }
