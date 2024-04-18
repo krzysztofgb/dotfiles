dd27c185be444cf030d059e64f6de8d72c81f379
Krzysztof Gasienica-Bednarz <krzysztofgb@yahoo.com>
2024-04-18 16:35:14 -0500

    Add dashboard home func

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -137,6 +137,16 @@ return {
           starter.refresh()
         end,
       })
+
+      local go_home = function()
+        local wins = vim.api.nvim_tabpage_list_wins(0)
+        if #wins > 1 and vim.bo[vim.api.nvim_win_get_buf(wins[1])].filetype == "neo-tree" then
+          vim.fn.win_gotoid(wins[2]) -- go to non-neo-tree window to toggle dashboardf
+        end
+        starter.open()
+      end
+      vim.keymap.set("n", "<Leader>h", go_home, { desc = "Go [H]ome" })
+
     end,
   },
   { import = "astrocommunity.test.neotest" },
