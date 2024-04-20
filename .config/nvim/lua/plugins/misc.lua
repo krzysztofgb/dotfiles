6f3a14837b886370cb9fdbb6e647e58cf025341d
Krzysztof Gasienica-Bednarz <krzysztofgb@yahoo.com>
2024-04-19 21:13:25 -0500

    Fix tmux navigation, telescope settings

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -1,46 +1,44 @@
 ---@type LazySpec
 return {
-	"andweeb/presence.nvim",
-	-- Detect tabstop and shiftwidth automatically
-	"tpope/vim-sleuth",
-	-- Game to practice vim motions
-	"ThePrimeagen/vim-be-good",
-	-- Cool animations
-	"eandrju/cellular-automaton.nvim",
-	{
-		"ray-x/lsp_signature.nvim",
-		event = "BufRead",
-		config = function()
-			require("lsp_signature").setup()
-		end,
-	},
-	{
-		"echasnovski/mini.nvim",
-		config = function()
-			-- Better Around/Inside textobjects
-			--
-			-- Examples:
-			--  - va)  - [V]isually select [A]round [)]paren
-			--  - yinq - [Y]ank [I]nside [N]ext [']quote
-			--  - ci'  - [C]hange [I]nside [']quote
-			require("mini.ai").setup({ n_lines = 500 })
-		end,
-	},
-	{
-		"christoomey/vim-tmux-navigator",
-		cmd = {
-			"TmuxNavigateLeft",
-			"TmuxNavigateDown",
-			"TmuxNavigateUp",
-			"TmuxNavigateRight",
-			"TmuxNavigatePrevious",
-		},
-		config = function()
-			vim.keymap.set("n", "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>")
-			vim.keymap.set("n", "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>")
-			vim.keymap.set("n", "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>")
-			vim.keymap.set("n", "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>")
-			vim.keymap.set("n", "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>")
-		end,
-	},
+  -- Rich Discord presence
+  "andweeb/presence.nvim",
+  -- Detect tabstop and shiftwidth automatically
+  "tpope/vim-sleuth",
+  -- Game to practice vim motions
+  "ThePrimeagen/vim-be-good",
+  -- Cool animations
+  "eandrju/cellular-automaton.nvim",
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    config = function() require("lsp_signature").setup() end,
+  },
+  {
+    "echasnovski/mini.nvim",
+    config = function()
+      -- Better Around/Inside textobjects
+      --
+      -- Examples:
+      --  - va)  - [V]isually select [A]round [)]paren
+      --  - yinq - [Y]ank [I]nside [N]ext [']quote
+      --  - ci'  - [C]hange [I]nside [']quote
+      require("mini.ai").setup { n_lines = 500 }
+    end,
+  },
+  {
+    "alexghergh/nvim-tmux-navigation",
+    config = function()
+      require("nvim-tmux-navigation").setup {
+        disable_when_zoomed = true, -- defaults to false
+        keybindings = {
+          left = "<C-h>",
+          down = "<C-j>",
+          up = "<C-k>",
+          right = "<C-l>",
+          last_active = "<C-\\>",
+          next = "<C-Space>",
+        },
+      }
+    end,
+  },
 }
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,44 +1,46 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	dependencies = {
-		"nvim-telescope/telescope-file-browser.nvim",
-		{ 
-			"nvim-telescope/telescope-live-grep-args.nvim" ,
-			-- This will not install any breaking changes.
-			-- For major updates, this must be adjusted manually.
-			version = "^1.0.0",
-		},
-	},
-	config = function(_, opts)
-		require("telescope").setup({
-			defaults = {
-				initial_mode = "insert",
-				path_display = {
-					"smart"
-				},
-			},
-			extensions = {
-				file_browser = {
-					hijack_netrw = true,
-					depth = false,
-				}
-			}
-		})
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-telescope/telescope-file-browser.nvim",
+    {
+      "nvim-telescope/telescope-live-grep-args.nvim",
+      -- This will not install any breaking changes.
+      -- For major updates, this must be adjusted manually.
+      version = "^1.0.0",
+    },
+  },
+  config = function(_, opts)
+    require("telescope").setup {
+      defaults = {
+        initial_mode = "insert",
+        path_display = {
+          "smart",
+        },
+        sorting_strategy = "descending",
+      },
+      extensions = {
+        file_browser = {
+          hijack_netrw = true,
+          depth = 5,
+          hidden = { file_browser = true, folder_browser = true },
+        },
+      },
+    }
 
-		require("telescope").load_extension("live_grep_args")
-		vim.keymap.set(
-			"n",
-			"<Leader>fg",
-			":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
-			{ desc = "[F]ind with [G]rep" }
-		)
+    require("telescope").load_extension "live_grep_args"
+    vim.keymap.set(
+      "n",
+      "<Leader>fg",
+      ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+      { desc = "[F]ind with [G]rep" }
+    )
 
-		require("telescope").load_extension("file_browser")
-		vim.keymap.set(
-			"n",
-			"<Leader>fe",
-			":Telescope file_browser path=%:p:h select_buffer=true<CR>",
-			{ desc = "[F]ile [E]xplorer" }
-		)
-	end,
+    require("telescope").load_extension "file_browser"
+    vim.keymap.set(
+      "n",
+      "<Leader>fe",
+      ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+      { desc = "[F]ile [E]xplorer" }
+    )
+  end,
 }
